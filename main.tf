33e0dfc48bd6633eceaacb35ef75cecf3264098f
nrayavaram <91076970+nrayavaram@users.noreply.github.com>
2021-10-20 11:51:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,15 +2,6 @@ resource "google_compute_firewall" "default" {
   name    = "test-firewall"
   network = google_compute_network.default.name
 
-  allow {
-    protocol = "icmp"
-  }
-  
-  deny {
-   protocol = "ssh"
-   ports    = ["22"]
-  }
-
   deny {
     protocol = "tcp"
     ports    = ["3389","80","8080"]
